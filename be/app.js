5fe89be91a521a1d79dda78483e6067d8d8d0bc2
yeynii <yeynii@gmail.com>
2022-10-13 15:50:57 +0900

    broadcast example

--- be/app.js
+++ be/app.js
@@ -8,10 +8,17 @@ const io = new Server({
 
 io.listen(3001);
 
+
+
+//https://socket.io/docs/v3/broadcasting-events/
 io.on("connection", (socket) => {
   // receive a message from the client
   socket.on("hello from client", (...args) => {
     console.log(args);
-    socket.emit("hello from server", ...args);
+    //sender socket을 제외한 나머지 클라이언트에게 broadcast
+    //socket.broadcast.emit("hello from server", ...args);
+    
+    //해당 소켓에 연결된 모든 클라이언트에게
+    io.emit("hello from server", ...args);
   });
 });
